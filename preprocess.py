6e60836eb72ca23d0bf090884b94c9035ee4a809
taneishi <taneishi@users.noreply.github.com>
2021-05-13 19:15:21 +0900

    renamed bond dict.

--- preprocess.py
+++ preprocess.py
@@ -16,20 +16,20 @@ def create_atoms(mol, atom_dict):
     atoms = [atom_dict[a] for a in atoms]
     return np.array(atoms)
 
-def create_ijbonddict(mol, bond_dict):
+def create_ij_bond_dict(mol, bond_dict):
     '''Create a dictionary, in which each key is a node ID
     and each value is the tuples of its neighboring node
     and chemical bond (e.g., single and double) IDs.
     '''
-    i_jbond_dict = defaultdict(lambda: [])
+    ij_bond_dict = defaultdict(lambda: [])
     for b in mol.GetBonds():
         i, j = b.GetBeginAtomIdx(), b.GetEndAtomIdx()
         bond = bond_dict[str(b.GetBondType())]
-        i_jbond_dict[i].append((j, bond))
-        i_jbond_dict[j].append((i, bond))
-    return i_jbond_dict
+        ij_bond_dict[i].append((j, bond))
+        ij_bond_dict[j].append((i, bond))
+    return ij_bond_dict
 
-def extract_fingerprints(radius, atoms, i_jbond_dict, fingerprint_dict, edge_dict):
+def extract_fingerprints(radius, atoms, ij_bond_dict, fingerprint_dict, edge_dict):
     '''Extract the fingerprints from a molecular graph
     based on Weisfeiler-Lehman algorithm.
     '''
@@ -37,14 +37,14 @@ def extract_fingerprints(radius, atoms, i_jbond_dict, fingerprint_dict, edge_dic
         nodes = [fingerprint_dict[a] for a in atoms]
     else:
         nodes = atoms
-        i_jedge_dict = i_jbond_dict
+        ij_edge_dict = ij_bond_dict
 
         for _ in range(radius):
             '''Update each node ID considering its neighboring nodes and edges.
             The updated node IDs are the fingerprint IDs.
             '''
             nodes_ = []
-            for i, j_edge in i_jedge_dict.items():
+            for i, j_edge in ij_edge_dict.items():
                 neighbors = [(nodes[j], edge) for j, edge in j_edge]
                 fingerprint = (nodes[i], tuple(sorted(neighbors)))
                 nodes_.append(fingerprint_dict[fingerprint])
@@ -52,15 +52,15 @@ def extract_fingerprints(radius, atoms, i_jbond_dict, fingerprint_dict, edge_dic
             '''Also update each edge ID considering
             its two nodes on both sides.
             '''
-            i_jedge_dict_ = defaultdict(lambda: [])
-            for i, j_edge in i_jedge_dict.items():
+            ij_edge_dict_ = defaultdict(lambda: [])
+            for i, j_edge in ij_edge_dict.items():
                 for j, edge in j_edge:
                     both_side = tuple(sorted((nodes[i], nodes[j])))
                     edge = edge_dict[(both_side, edge)]
-                    i_jedge_dict_[i].append((j, edge))
+                    ij_edge_dict_[i].append((j, edge))
 
             nodes = nodes_
-            i_jedge_dict = i_jedge_dict_
+            ij_edge_dict = ij_edge_dict_
 
     return np.array(nodes)
 
@@ -84,8 +84,8 @@ def create_dataset(args):
             mol = Chem.AddHs(Chem.MolFromSmiles(smiles))
             atoms = create_atoms(mol, atom_dict)
             molecular_size = len(atoms)
-            i_jbond_dict = create_ijbonddict(mol, bond_dict)
-            fingerprints = extract_fingerprints(args.radius, atoms, i_jbond_dict, fingerprint_dict, edge_dict)
+            ij_bond_dict = create_ij_bond_dict(mol, bond_dict)
+            fingerprints = extract_fingerprints(args.radius, atoms, ij_bond_dict, fingerprint_dict, edge_dict)
             adjacency = Chem.GetAdjacencyMatrix(mol)
 
             dataset.append((fingerprints, adjacency, molecular_size, property))
