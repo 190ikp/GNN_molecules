2234c438fea560ac0e7c0191692278a31c8b892e
taneishi <taneishi@users.noreply.github.com>
2021-06-09 04:23:30 -0700

    removed user id.

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#PBS -l nodes=1:gold6258r:ppn=1
+#PBS -N gnn
+#PBS -j oe
+#PBS -o output.log
+
+cd ${PBS_O_WORKDIR}
+
+KMP_SETTING="KMP_AFFINITY=granularity=fine,compact,1,0"
+TOTAL_CORES=28
+
+export ${KMP_SETTING}
+echo -e "### using ${KMP_SETTING}"
+
+export OMP_NUM_THREADS=${TOTAL_CORES}
+echo -e "### using OMP_NUM_THREADS=${TOTAL_CORES}"
+
+PYTHON=${HOME}/anaconda3/envs/torch/bin/python
+
+${PYTHON} preprocess.py
+${PYTHON} main.py --epochs 10
