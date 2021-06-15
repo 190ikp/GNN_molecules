0fcc1cd7fc7c376c157fce41efcea739f5a18e2c
taneishi <taneishi@users.noreply.github.com>
2021-06-14 17:13:18 -0700

    appended core calculation.

--- run.sh
+++ run.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-#PBS -l nodes=1:gold6258r:ppn=1
+#PBS -l nodes=1:gold5120:ppn=1
 #PBS -N gnn
 #PBS -j oe
 #PBS -o output.log
 
 cd ${PBS_O_WORKDIR}
 
-KMP_SETTING="KMP_AFFINITY=granularity=fine,compact,1,0"
-TOTAL_CORES=28
+CPUS=$(cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l)
+CORES=$(cat /proc/cpuinfo | grep "core id" | sort | uniq | wc -l)
+TOTAL_CORES=$(expr ${CPUS} \\* ${CORES})
 
-export ${KMP_SETTING}
-echo -e "### using ${KMP_SETTING}"
-
+echo "CPUS=${CPUS} CORES=${CORES} TOTAL_CORES=${TOTAL_CORES}"
 export OMP_NUM_THREADS=${TOTAL_CORES}
 echo -e "### using OMP_NUM_THREADS=${TOTAL_CORES}"
+
+KMP_SETTING="KMP_AFFINITY=granularity=fine,compact,1,0"
+export ${KMP_SETTING}
+echo -e "### using ${KMP_SETTING}"
 
 PYTHON=${HOME}/anaconda3/envs/torch/bin/python
 
 ${PYTHON} preprocess.py
-${PYTHON} main.py --epochs 10
+${PYTHON} main.py --epochs 5
