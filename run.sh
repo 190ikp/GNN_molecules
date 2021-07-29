87ea25d19e276fa9b2f00ebe51ac0521c71b52b3
190ikp <ikp190@outlook.com>
2021-07-29 18:36:11 +0900

    fix script

--- run.sh
+++ run.sh
@@ -4,18 +4,18 @@
 #PBS -j oe
 #PBS -o output.log
 
-cd ${PBS_O_WORKDIR}
+cd "${PBS_O_WORKDIR}" || exit
 
-CPUS=$(cat /proc/cpuinfo | grep "physical id" | sort | uniq | wc -l)
-CORES=$(cat /proc/cpuinfo | grep "core id" | sort | uniq | wc -l)
-TOTAL_CORES=$(expr ${CPUS} \\* ${CORES})
+CPUS=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
+CORES=$(grep "core id" /proc/cpuinfo | sort | uniq | wc -l)
+TOTAL_CORES=$(("${CPUS}" * "${CORES}"))
 
 echo "CPUS=${CPUS} CORES=${CORES} TOTAL_CORES=${TOTAL_CORES}"
 export OMP_NUM_THREADS=${TOTAL_CORES}
 echo -e "### using OMP_NUM_THREADS=${TOTAL_CORES}"
 
 KMP_SETTING="KMP_AFFINITY=granularity=fine,compact,1,0"
-export ${KMP_SETTING}
+export KMP_SETTING
 echo -e "### using ${KMP_SETTING}"
 
 PYTHON=${HOME}/.conda/envs/torch/bin/python
